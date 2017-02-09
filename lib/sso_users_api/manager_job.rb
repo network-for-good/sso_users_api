80885f4a9f429581d874411bed0b4a2c0ff6c2e1
Thomas Hoen <tom@givecorps.om>
2017-02-09 16:55:26 -0500

    Retries job if it rails. Will stop after the 3rd attempt

--- lib/sso_users_api/manager_job.rb
+++ lib/sso_users_api/manager_job.rb
@@ -5,16 +5,18 @@ module SsoUsersApi
     queue_as :sso_users_api
 
     def perform(id, class_name, count = 0)
-      # do not attempt to perform the operation again if the count has exceeded 3
-      return if count == 3
 
       user = class_name.constantize.find(id)
       SsoUsersApi::Manager.new(user).call
+
     rescue StandardError => e
-      sleep 5 * count
-      self.class.perform_later(id, class_name, count + 1)
+      # do not attempt to perform the operation again if this is the third attempt
+      if count >= 2
+        raise
+      else
+        sleep 5 * count
+        self.class.perform_later(id, class_name, count + 1)
+      end
     end
-
   end
-
 end
--- lib/sso_users_api/token.rb
+++ lib/sso_users_api/token.rb
@@ -1,15 +1,17 @@
 module SsoUsersApi
   class Token < Flexirest::Base
-    base_url "https://identity-qa.networkforgood.org/connect"
+    verbose true
+    base_url Rails.env.production? ? "https://identity.networkforgood.org/connect" : "https://identity-qa.networkforgood.org/connect"
 
     def initialize
       super
       self.scope = 'donation idmgr'
       self.grant_type = 'client_credentials'
     end
-    verbose!
+
 
     post :create, "/token"
+
   end
 
 end
