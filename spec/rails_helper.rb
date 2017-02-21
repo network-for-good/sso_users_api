6e507d45be57f6f704ba1aca965f56f2b56404aa
Thomas Hoen <tom@givecorps.om>
2017-02-21 16:56:47 -0500

    Clean up rspec configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,18 @@ require 'spec_helper'
 require 'webmock/rspec'
 require 'vcr'
 
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+Rails.backtrace_cleaner.remove_silencers!
+
+#
+# Load support files
+ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
+Dir[File.join(ENGINE_RAILS_ROOT, "spec/support/**/*.rb")].each {|f| require f }
+
+RSpec.configure do |config|
+  config.order = "random"
+end
+
 VCR.configure do |config|
  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
  config.hook_into :webmock
