a06f66ad8399e7a2043c3f10ed8b759fe7c76296
Thomas Hoen <tom@givecorps.om>
2017-03-08 11:53:52 -0500

    updates the user flexirest model to use the openid Urls methods

--- lib/sso_users_api/user.rb
+++ lib/sso_users_api/user.rb
@@ -1,16 +1,14 @@
 module SsoUsersApi
   class User < Base
 
-
     # before using, the access_token must be set on the base class, i.e
     # SsoUsersApi::Base.access_token [access_token]
 
-    base_url ::Rails.env.production? ? "https://users.networkforgood.org/api" : "https://users-qa.networkforgood.org/api"
-    verbose true
+    base_url SsoOpenid::Urls.identity_users.fqdn
 
-    get :all, "/user"
-    get :find, "/user/:id"
-    put :update, "/user/:id"
-    post :create, "/user"
+    get :all, "api/user"
+    get :find, "api/user/:id"
+    put :update, "api/user/:id"
+    post :create, "api/user"
   end
 end
