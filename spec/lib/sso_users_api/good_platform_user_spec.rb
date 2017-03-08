90d1ff9fbb04b07f830cf9840a8684a902d98237
Thomas Hoen <tom@givecorps.om>
2017-03-08 11:56:11 -0500

    updates specs

---
+++ spec/lib/sso_users_api/good_platform_user_spec.rb
@@ -0,0 +1,66 @@
+require "rails_helper"
+
+describe SsoUsersApi::GoodPlatformUser do
+  let(:klass) { SsoUsersApi::GoodPlatformUser }
+
+  let(:good_platform_user) { klass.new(user) }
+  let(:user) { OpenStruct.new(email: email) }
+  let(:email) { "this@that.com" }
+
+  describe ".call" do
+    subject { klass.call(user) }
+
+    it "should create a new good_platform_user instance, passing in user, and calling call" do
+      call_double = double
+      expect(call_double).to receive(:call).and_return([])
+      expect(klass).to receive(:new).with(user).and_return(call_double)
+      subject
+    end
+  end
+
+  describe "#call" do
+    subject { good_platform_user.call }
+
+    it "should call the UsersApplicationAccessList list method" do
+      expect(SsoUsersApi::UserApplicationAccessList).to receive(:list).with(email: user.email).and_return(double(items: []))
+      subject
+    end
+
+    context 'when the results of the user application access list returns a 404, not found response' do
+      it "should return an empty array" do
+        VCR.use_cassette('user_with_no_access_records') do
+          expect(subject).to eq([])
+        end
+      end
+    end
+
+    context 'when the results of the user application access returns results, but has no gp records' do
+      let(:email) { "tom@givecorps.com" }
+      it "should return an empty array" do
+        VCR.use_cassette('user_with_access_records_but_no_gp_records') do
+          SsoUsersApi::UserApplicationAccessList.list email: user.email
+          # expect(subject).to eq([])
+        end
+      end
+    end
+
+    context 'when the results of the user application access returns results, with gp records, but the user is not active' do
+      let(:email) { "nfgcvlx+27-1095214D@gmail.com" }
+      it "should return an empty array" do
+        VCR.use_cassette('user_with_gp_access_records_all_are_inactive') do
+          # expect(subject).to eq([])
+          SsoUsersApi::UserApplicationAccessList.list email: "tom@givecorps.com"
+        end
+      end
+    end
+
+    context 'when the results of the user application access call returns results, with gp records, but the organization is not active' do
+      let(:email) { "nfgcvlx+27-1095214D@gmail.com" }
+      it "should return an empty array" do
+        VCR.use_cassette('user_with_gp_access_records_all_orgs_are_inactive') do
+          expect(subject).to eq([])
+        end
+      end
+    end
+  end
+end
